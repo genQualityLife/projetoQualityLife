4bf4ff0d3bc79a10c4636e7266bae387ab2ad5d6
Julianne Pereira Santos Pinheiro <jupsantos@outlook.com.br>
2021-11-04 10:20:56 -0300

    Atualização do DER

--- Documentacao/QualityLife_mysql.sql
+++ Documentacao/QualityLife_mysql.sql
@@ -1,11 +1,9 @@
 CREATE TABLE `tb_produto` (
 	`id_produto` bigint NOT NULL AUTO_INCREMENT,
 	`nome_produto` varchar(20) NOT NULL,
-	`imagens_produto` varbinary(4) NOT NULL,
-	`quantidade` int(50) NOT NULL,
+	`imagens_produto` varchar(255) NOT NULL,
 	`descricao` varchar(255) NOT NULL,
 	`preco` DECIMAL(7,2) NOT NULL,
-	`risco` int(10) NOT NULL,
 	`fk_id_categoria` bigint(4) NOT NULL,
 	`fk_id_usuario` bigint(4) NOT NULL,
 	PRIMARY KEY (`id_produto`)
@@ -14,7 +12,7 @@ CREATE TABLE `tb_produto` (
 CREATE TABLE `tb_categoria` (
 	`id_categoria` bigint(4) NOT NULL AUTO_INCREMENT,
 	`nome_categoria` varchar(20) NOT NULL,
-	`imagem` varbinary(4) NOT NULL,
+	`imagem` varchar(255) NOT NULL,
 	`descricao` varchar(255) NOT NULL,
 	PRIMARY KEY (`id_categoria`)
 );
