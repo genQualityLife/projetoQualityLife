37ae55837303b08445a8e2d7420cf6dbc47e31bf
Julianne Pereira Santos Pinheiro <jupsantos@outlook.com.br>
2021-12-03 19:01:58 -0300

    Implementacao dos componentes menu, rodape, login e cadastro

---
+++ front-end/qualityLife/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    CadastroComponent,
+    LoginComponent,
+    ProdutosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
