3f44ec2ef67540c4c5fd31b22327acfea8ea190e
Julianne Pereira Santos Pinheiro <jupsantos@outlook.com.br>
2021-12-05 12:41:51 -0300

    Implementacao de produtos

--- front-end/qualityLife/src/app/app-routing.module.ts
+++ front-end/qualityLife/src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
+import { ProdutosComponent } from './produtos/produtos.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
 
   {path: 'login', component: LoginComponent},
-  {path: 'cadastro', component: CadastroComponent}
-  
+  {path: 'cadastro', component: CadastroComponent},
+  {path: 'produtos', component: ProdutosComponent}
+
 ];
 
 @NgModule({
